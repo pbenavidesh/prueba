c9a8c995de713a202a14a8d2cf4106c6b3da4552
Pablo <pbenavides@iteso.mx>
2023-08-18 08:51:44 -0600

    added rscript

--- primer_script.R
+++ primer_script.R
@@ -1,2 +1,19 @@
 # mi primer R Script
 library(tidyverse)
+
+write_csv(mpg, "datos/mpg.csv")
+
+#siyoescriboasinoseentiendetanto
+# ALT + "-" para poner la "flechita"
+df <- read_csv("datos/mpg.csv")
+
+# CTRL/CMD + SHIFT + M
+df |> 
+  distinct(manufacturer)
+
+distinct(df, manufacturer)
+
+df |> 
+  filter(manufacturer == "jeep",
+         cyl == 6) 
+
